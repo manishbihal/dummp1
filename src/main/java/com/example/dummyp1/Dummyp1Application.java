56be3f046cc6b654b455803261822304f85bafcc
Singh, Atin <atin@pragra.io>
2021-01-20 21:30:06 -0500

    Iniital version

---
+++ src/main/java/com/example/dummyp1/Dummyp1Application.java
@@ -0,0 +1,13 @@
+package com.example.dummyp1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Dummyp1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Dummyp1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/dummyp1/Dummyp1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dummyp1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Dummyp1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
